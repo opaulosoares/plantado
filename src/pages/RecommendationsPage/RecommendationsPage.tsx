763ffe91c9ff020ba3033acca90759aa33b66448
Paulo <paulosoares.other@gmail.com>
2023-12-01 14:41:21 -0300

    Functional grids

--- src/pages/RecommendationsPage/RecommendationsPage.tsx
+++ src/pages/RecommendationsPage/RecommendationsPage.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Stepper, Step, StepLabel, useEventCallback } from "@mui/material";
 import RecommendationSurveySection, {
-  Questions,
+    Questions,
 } from "../../components/RecommendationSurveySection/RecommendationSurveySection";
 import BasePage from "../../templates/BasePage/BasePage";
 import { SurveyQuestions } from "./survey-questions";
@@ -13,101 +13,106 @@ import { useNavigate } from "react-router-dom";
 type StepType = 0 | 1 | 2;
 
 const generateProductsId = (products: any[], quantity: number): any[] => {
-  const allProductIds = products.map((product) => product.id);
-  const recommendedProcutIds = new Set();
+    const allProductIds = products.map((product) => product.id);
+    const recommendedProcutIds = new Set();
 
-  while (recommendedProcutIds.size < quantity) {
-    const randomId = Math.round(Math.random() * (allProductIds.length - 1));
-    recommendedProcutIds.add(allProductIds[randomId]);
-  }
+    while (recommendedProcutIds.size < quantity) {
+        const randomId = Math.round(Math.random() * (allProductIds.length - 1));
+        recommendedProcutIds.add(allProductIds[randomId]);
+    }
 
-  return Array.from(recommendedProcutIds);
+    return Array.from(recommendedProcutIds);
 };
 
 function RecommendationsPage() {
-  const navigate = useNavigate();
-  const [currentStep, setCurrentStep] = useState<StepType>(0);
-  const [surveyQuestions, setSurveyQuestions] = useState(SurveyQuestions);
-  const stepperSteps = useMemo(
-    () => [...SurveyQuestions.map((survey) => survey.title), "Recomendações"],
-    []
-  );
-  const [recommendedProductsIds, setRecommendedProductsIds] = useState(
-    generateProductsId(db.produtos.plantas, 3)
-  );
-
-  const handleSurveyChanges = (title: string, questions: Questions) => {
-    setSurveyQuestions((list) =>
-      list.map((item) =>
-        item.title === title ? { ...item, questions: questions } : item
-      )
+    const navigate = useNavigate();
+    const [currentStep, setCurrentStep] = useState<StepType>(0);
+    const [surveyQuestions, setSurveyQuestions] = useState(SurveyQuestions);
+    const stepperSteps = useMemo(
+        () => [
+            ...SurveyQuestions.map((survey) => survey.title),
+            "Recomendações",
+        ],
+        []
     );
-  };
+    const [recommendedProductsIds, setRecommendedProductsIds] = useState(
+        generateProductsId(db.produtos.plantas, 3)
+    );
 
-  const handleGoNext = () => {
-    setCurrentStep((prev) => Math.min(prev + 1, 2) as StepType);
-  };
+    const handleSurveyChanges = (title: string, questions: Questions) => {
+        setSurveyQuestions((list) =>
+            list.map((item) =>
+                item.title === title ? { ...item, questions: questions } : item
+            )
+        );
+    };
 
-  const handleGoBack = () => {
-    setCurrentStep((prev) => Math.max(prev - 1, 0) as StepType);
-  };
+    const handleGoNext = () => {
+        setCurrentStep((prev) => Math.min(prev + 1, 2) as StepType);
+    };
 
-  const handleResetTest = () => {
-    setSurveyQuestions(SurveyQuestions);
-    setCurrentStep(0);
-    setRecommendedProductsIds(generateProductsId(db.produtos.plantas, 3));
-  };
+    const handleGoBack = () => {
+        setCurrentStep((prev) => Math.max(prev - 1, 0) as StepType);
+    };
 
-  const handleBackHome = useEventCallback(() => {
-    navigate("/");
-  }, []);
+    const handleResetTest = () => {
+        setSurveyQuestions(SurveyQuestions);
+        setCurrentStep(0);
+        setRecommendedProductsIds(generateProductsId(db.produtos.plantas, 3));
+    };
 
-  const stepperComponent = useMemo(() => {
-    if ([0, 1].includes(currentStep)) {
-      const surveySection = surveyQuestions[currentStep];
+    const handleBackHome = useEventCallback(() => {
+        navigate("/");
+    }, []);
 
-      return (
-        <RecommendationSurveySection
-          key={surveySection.title}
-          questions={surveySection.questions}
-          onChange={(newState) =>
-            handleSurveyChanges(surveySection.title, newState)
-          }
-          handleGoNext={handleGoNext}
-          handleGoBack={currentStep > 0 ? handleGoBack : undefined}
-        />
-      );
-    } else if (currentStep === 2) {
-      return (
-        <RecommendedProducts
-          productIds={recommendedProductsIds as [number, number, number]}
-          handleResetTest={handleResetTest}
-          handleBackHome={handleBackHome}
-        />
-      );
-    }
+    const stepperComponent = useMemo(() => {
+        if ([0, 1].includes(currentStep)) {
+            const surveySection = surveyQuestions[currentStep];
 
-    return <span>Error</span>;
-  }, [currentStep, handleBackHome, recommendedProductsIds, surveyQuestions]);
+            return (
+                <RecommendationSurveySection
+                    key={surveySection.title}
+                    questions={surveySection.questions}
+                    onChange={(newState) =>
+                        handleSurveyChanges(surveySection.title, newState)
+                    }
+                    handleGoNext={handleGoNext}
+                    handleGoBack={currentStep > 0 ? handleGoBack : undefined}
+                />
+            );
+        } else if (currentStep === 2) {
+            return (
+                <RecommendedProducts
+                    productIds={
+                        recommendedProductsIds as [number, number, number]
+                    }
+                    handleResetTest={handleResetTest}
+                    handleBackHome={handleBackHome}
+                />
+            );
+        }
 
-  return (
-    <BasePage>
-      <Stepper
-        activeStep={currentStep + 1}
-        alternativeLabel
-        sx={{ marginBottom: 5, marginTop: 3 }}
-      >
-        {stepperSteps.map((title) => (
-          <Step key={title}>
-            <StepLabel>{title}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <section style={{ marginTop: 16, marginBottom: 16 }}>
-        {stepperComponent}
-      </section>
-    </BasePage>
-  );
+        return <span>Error</span>;
+    }, [currentStep, handleBackHome, recommendedProductsIds, surveyQuestions]);
+
+    return (
+        <BasePage>
+            <Stepper
+                activeStep={currentStep}
+                alternativeLabel
+                sx={{ marginBottom: 5, marginTop: 3 }}
+            >
+                {stepperSteps.map((title) => (
+                    <Step key={title}>
+                        <StepLabel>{title}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+            <section style={{ marginTop: 16, marginBottom: 16 }}>
+                {stepperComponent}
+            </section>
+        </BasePage>
+    );
 }
 
 export default RecommendationsPage;
