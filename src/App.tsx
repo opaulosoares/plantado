dc41a89056556e4372fb3a9c38c04de128b0c82a
Matheus Bermudes Viana <62019868+MatheusBViana@users.noreply.github.com>
2023-12-01 18:48:29 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import RecommendationsPage from "./pages/RecommendationsPage/RecommendationsPage
 import OrderConfirmationPage from "./pages/SuccessPage/OrderConfirmationPage";
 import CartPage from "./pages/CartPage/CartPage";
 import ProductCategoriesPage from "./pages/ProductsCategoriesPage/ProductsPage";
-
+import ProductPage from "./pages/ProductPage/ProductPage"
 function App() {
     const [theme, colorMode] = useMode();
 
@@ -34,6 +34,7 @@ function App() {
                             element={<OrderConfirmationPage />}
                         />
                         <Route path="/cart" element={<CartPage />} />
+                        <Route path="/product" element={<ProductPage />} />
                     </Routes>
                 </Router>
             </ThemeProvider>
