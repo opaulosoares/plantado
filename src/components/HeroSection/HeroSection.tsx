f7e557cdf7494b690cf495907d553646ee5178ca
gustavo alves <gustavo_alves2010@yahoo.com.br>
2023-12-01 13:50:23 -0300

    feat add recommendations page

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -3,49 +3,55 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 interface HeroSectionProps {
-    primaryText: string;
-    secondaryText: string;
-    callToAction: string;
-    actionRoute: string;
+  primaryText: string;
+  secondaryText: string;
+  callToActionList: Array<{
+    text: string;
+    path: string;
+    variant: 'primary' | 'secondary';
+  }>;
 }
 
 const HeroSection: React.FC<HeroSectionProps> = ({
-    primaryText,
-    secondaryText,
-    callToAction,
-    actionRoute,
+  primaryText,
+  secondaryText,
+  callToActionList,
 }) => {
-    return (
-        <Stack
-            sx={{
-                py: { xs: 12, md: 16 },
-                justifyContent: "center",
-                alignItems: "center",
-                gap: 4,
-                textAlign: "center",
-            }}
-        >
-            <Stack
-                sx={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    gap: 1,
-                }}
-            >
-                <Typography variant="h1" component="h1" tabIndex={0}>
-                    {primaryText}
-                </Typography>
-                <Typography variant="h3" component="p" tabIndex={0}>
-                    {secondaryText}
-                </Typography>
-            </Stack>
-            <Link to={actionRoute}>
-                <Button variant="contained" color="primary">
-                    {callToAction}
-                </Button>
-            </Link>
-        </Stack>
-    );
+  return (
+    <Stack
+      sx={{
+        py: { xs: 12, md: 16 },
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 4,
+        textAlign: "center",
+      }}
+    >
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 1,
+        }}
+      >
+        <Typography variant="h1" component="h1" tabIndex={0}>
+          {primaryText}
+        </Typography>
+        <Typography variant="h3" component="p" tabIndex={0}>
+          {secondaryText}
+        </Typography>
+      </Stack>
+      <Stack direction={"row"} sx={{ gap: 1 }}>
+        {callToActionList.map((item) => (
+          <Link key={item.text} to={item.path}>
+            <Button variant="contained" color={item.variant}>
+              {item.text}
+            </Button>
+          </Link>
+        ))}
+      </Stack>
+    </Stack>
+  );
 };
 
 export default HeroSection;
---
+++ src/components/RecommendationSurveySection/RecommendationSurveySection.tsx
@@ -0,0 +1,94 @@
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Stack,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+
+export type Questions = Array<{
+  question: string;
+  selectedAnswer?: string;
+  answers: Array<{
+    text: string;
+    value: string;
+  }>;
+}>;
+
+export interface Props {
+  questions: Questions;
+  onChange: (state: Questions) => void;
+  handleGoNext: VoidFunction;
+  handleGoBack?: VoidFunction;
+}
+
+function RecommendationSurveySection(props: Readonly<Props>) {
+  const { questions, onChange, handleGoNext, handleGoBack } = props;
+  const [canGoNext, setCanGoForward] = useState(false);
+
+  useEffect(() => {
+    setCanGoForward(questions.every((question) => !!question.selectedAnswer));
+  }, [questions]);
+
+  const handleOnRadiOGroupChanges = (question: string, value: string) => {
+    const newQuestions = questions.map((item) =>
+      item.question === question ? { ...item, selectedAnswer: value } : item
+    );
+    onChange(newQuestions);
+  };
+
+  return (
+    <form>
+      <Stack sx={{ gap: 3, alignItems: "center" }}>
+        {questions.map((item) => (
+          <FormControl
+            key={item.question}
+            sx={{ gap: 1, alignItems: "center" }}
+          >
+            <FormLabel>{item.question}</FormLabel>
+            <RadioGroup
+              row
+              name={item.question}
+              value={item.selectedAnswer ?? null}
+              onChange={(e) =>
+                handleOnRadiOGroupChanges(item.question, e.target.value)
+              }
+              sx={{ dispaly: "flex", alignItems: "center" }}
+            >
+              {item.answers.map((item2) => (
+                <FormControlLabel
+                  key={item2.value}
+                  value={item2.value}
+                  control={<Radio />}
+                  label={item2.text}
+                />
+              ))}
+            </RadioGroup>
+          </FormControl>
+        ))}
+        <Stack
+          direction="row"
+          sx={{ justifyContent: "space-evenly", width: "100%", marginTop: 2 }}
+        >
+          {handleGoBack && (
+            <Button variant="contained" onClick={handleGoBack}>
+              Voltar
+            </Button>
+          )}
+          <Button
+            variant="contained"
+            onClick={handleGoNext}
+            disabled={!canGoNext}
+          >
+            Proximo
+          </Button>
+        </Stack>
+      </Stack>
+    </form>
+  );
+}
+
+export default RecommendationSurveySection;
