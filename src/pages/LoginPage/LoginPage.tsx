8fd221f9afd77bcd71e6d0e8ced3a616a1b82364
Matheus Bermudes Viana <62019868+MatheusBViana@users.noreply.github.com>
2023-12-01 16:06:34 -0300

    Update LoginPage.tsx

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,71 +1,140 @@
+
+import React, { useContext, useState } from "react";
 import {
-    InputLabel,
-    Stack,
-    TextField,
-    Typography,
-    useTheme,
+  useTheme,
+  Typography,
+  Button,
+  Box,
+  Divider,
+  TextField,
+  Grid,
+  Paper,
+  Checkbox,
 } from "@mui/material";
-import React from "react";
-import PlantadoLogo from "../../components/PlantadoLogo/PlantadoLogo";
-import { spacing, tokens } from "../../theme";
-import { NavLink } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import AccessibilityBar from "../../components/AccessibilityBar/AccessbilityBar";
+import { ColorModeContext, spacing, tokens } from "../../theme";
+import PlantadoLogo from "../PlantadoLogo/PlantadoLogo";
+
 
 const LoginPage: React.FC = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    return (
-        <Stack
-            py={8}
-            px={spacing.GLOBAL_HORIZONTAL_MARGIN}
-            justifyContent={"center"}
-            alignItems={"center"}
-            width={"100%"}
-            gap={8}
+const colorMode = useContext(ColorModeContext);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [rememberMe, setRememberMe] = React.useState(false);
+
+  const handleLogin = () => {
+    // Adicionar lógica para lidar com o login
+  };
+
+  const handleForgotPassword = () => {
+    // Adicionar lógica para redefinir a senha
+  };
+
+  return (
+    <>
+      <header>
+        <AccessibilityBar />
+        <Navbar />
+      </header>
+      <main>
+        <Box
+        sx={{
+            width: '50%',
+            height: '20%',
+            margin: '2% auto',
+            textAlign: 'center',
+            tabindex: "0"
+
+        }}>
+        <PlantadoLogo 
+                        colorText={colors.neutral[12]}
+                        style={{ width: 450 }}
+                        
+                    />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            height: "80vh",
+            width: "70%",
+            padding: "3% 0%",
+            margin: '0 auto',
+
+            backgroundColor: colors.grass[1],
+            boxShadow: "5"
+          }}
         >
-            <NavLink to={"/"} role="button">
-                <PlantadoLogo
-                    colorText={colors.neutral[12]}
-                    style={{ width: 256 }}
-                />
-            </NavLink>
-            <Stack
-                id="desktop-login"
-                sx={{ display: { xs: "none", md: "flex" } }}
-                width={"100%"}
+          <Box
+            sx={{
+              width: "90%",
+              display: "flex",
+            //   backgroundColor: "#4CAF50", // Fundo verde
+            }}
+          >
+            {/* Lado Esquerdo - Fundo Verde */}
+            <Box sx={{ width: "60%", padding: "20px", backgroundColor: colors.grass[10]}}>
+              {/* Adicione qualquer conteúdo adicional do lado esquerdo aqui */}
+            </Box>
+
+            {/* Lado Direito - Sessão de Login */}
+            <Box
+              sx={{
+                width: "50%",
+                padding: "20px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                marginTop: "15%",
+                backgroundColor: colors.grass[1]
+              }}
             >
-                <Stack
-                    width={"100%"}
-                    sx={{
-                        borderRadius: 2,
-                        boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.15)",
-                    }}
-                    direction={"row"}
+              <Typography tabIndex="0" variant="h4" gutterBottom>
+                Faça Login
+              </Typography>
+              <form>
+                <TextField
+                  fullWidth
+                  label="E-mail"
+                  variant="outlined"
+                  margin="normal"
+                  required
+                />
+                <TextField
+                  fullWidth
+                  label="Senha"
+                  variant="outlined"
+                  margin="normal"
+                  type="password"
+                  required
+                />
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <Checkbox
+                    checked={rememberMe}
+                    onChange={() => setRememberMe(!rememberMe)}
+                  />
+                  <Typography> Lembrar Dados</Typography>
+                </Box>
+                <Button sx={{width: "100%"}} variant="contained" onClick={handleLogin}>
+                  Entrar
+                </Button>
+                <Typography
+                  sx={{ cursor: "pointer", marginTop: "10px", tabIndex: "0" }}
+                  onClick={handleForgotPassword}
                 >
-                    <Stack p={2} width={"50%"}>
-                        <Stack
-                            bgcolor={colors.grass[12]}
-                            width={"100%"}
-                            height={"100%"}
-                            justifyContent={"center"}
-                            alignItems={"center"}
-                            sx={{ borderRadius: "8px 0px 0px 8px" }}
-                        >
-                            <Typography
-                                variant={"h1"}
-                                color={colors.neutral[1]}
-                            >
-                                Arte aleatoria
-                            </Typography>
-                        </Stack>
-                    </Stack>
-                    <Stack p={4}>
-                        <InputLabel>Teste</InputLabel>
-                        <TextField />
-                    </Stack>
-                </Stack>
-            </Stack>
-        </Stack>
-    );
+                  Esqueceu a senha? Redefina aqui
+                </Typography>
+              </form>
+              <Button sx={{ width: "100%", marginTop: "10px", marginRight: "45%" }}>
+                Não é cliente? Cadastre-se aqui
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </main>
+    </>
+  );
 };
 
 export default LoginPage;
