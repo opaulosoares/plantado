f9d35d2f492117a221848327c91fe8e8a5d4bf5f
gustavo alves <gustavo_alves2010@yahoo.com.br>
2023-12-01 22:28:43 -0300

    feat: controll cart drawer

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,111 +2,125 @@ import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
 
 // Definindo tipos
 export interface User {
-    _id?: string;
-    name?: string;
-    email?: string;
+  _id?: string;
+  name?: string;
+  email?: string;
 }
 
 export interface Product {
-    id: number;
-    name: string;
-    description: string;
-    price: number;
-    discountedPrice: number;
-    hasDiscount: boolean;
-    image: string;
-    alt_text: string;
-    subcategory: string;
-    category: string;
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  discountedPrice: number;
+  hasDiscount: boolean;
+  image: string;
+  alt_text: string;
+  subcategory: string;
+  category: string;
+}
+
+export interface CartProduct extends Product {
+  qty: number;
 }
 
 interface AppState {
-    isLoggedIn: boolean;
-    user: User;
-    cart: Product[];
-    address?: {
-        street: string;
-        number: number;
-        city: string;
-        state: string;
-        country: string;
-        zipCode: string;
-    };
-    products: { [key: string]: Product };
-    users: { [key: string]: User };
-    categoryData: { [key: string]: any };
-    bestDeals: { [key: string]: any };
+  isLoggedIn: boolean;
+  user: User;
+  cart: Product[];
+  address?: {
+    street: string;
+    number: number;
+    city: string;
+    state: string;
+    country: string;
+    zipCode: string;
+  };
+  products: { [key: string]: Product };
+  users: { [key: string]: User };
+  categoryData: { [key: string]: any };
+  bestDeals: { [key: string]: any };
 }
 
 // Função para carregar o estado do localStorage
 const loadState = (): AppState | undefined => {
-    try {
-        const serializedState = localStorage.getItem("appState");
-        if (serializedState === null) {
-            return undefined;
-        }
-        return JSON.parse(serializedState);
-    } catch (err) {
-        return undefined;
+  try {
+    const serializedState = localStorage.getItem("appState");
+    if (serializedState === null) {
+      return undefined;
     }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
 };
 
 // Função para salvar o estado no localStorage
 const saveState = (state: AppState) => {
-    try {
-        const serializedState = JSON.stringify(state);
-        localStorage.setItem("appState", serializedState);
-    } catch (err) {
-        // Tratar erros ao salvar no localStorage, se necessário
-    }
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("appState", serializedState);
+  } catch (err) {
+    // Tratar erros ao salvar no localStorage, se necessário
+  }
 };
 
 // Estado inicial
 const initialState: AppState = loadState() || {
-    isLoggedIn: true,
-    user: {
-        name: "John Doe",
-        email: "johndoe@mail.com",
-    },
-    cart: [],
-    products: {},
-    users: {},
-    categoryData: {},
-    bestDeals: {},
+  isLoggedIn: true,
+  user: {
+    name: "John Doe",
+    email: "johndoe@mail.com",
+  },
+  cart: [],
+  products: {},
+  users: {},
+  categoryData: {},
+  bestDeals: {},
 };
 
 // Criando um único slice para tudo
 const appSlice = createSlice({
-    name: "app",
-    initialState,
-    reducers: {
-        setUser: (state, action: PayloadAction<User>) => {
-            state.user = { ...state.user, ...action.payload };
-            saveState(state);
-        },
-        loggedIn: (state) => {
-            state.isLoggedIn = true;
-            saveState(state);
-        },
-        loggedOut: (state) => {
-            state.isLoggedIn = false;
-            saveState(state);
-        },
-        addToCart: (state, action: PayloadAction<Product>) => {
-            state.cart.push(action.payload);
-            saveState(state);
-        },
-        removeFromCart: (state, action: PayloadAction<number>) => {
-            state.cart = state.cart.filter(
-                (item) => item.id !== action.payload
-            );
-            saveState(state);
-        },
-        clearCart: (state) => {
-            state.cart = [];
-            saveState(state);
-        },
+  name: "app",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = { ...state.user, ...action.payload };
+      saveState(state);
     },
+    loggedIn: (state) => {
+      state.isLoggedIn = true;
+      saveState(state);
+    },
+    loggedOut: (state) => {
+      state.isLoggedIn = false;
+      saveState(state);
+    },
+    addToCart: (state, action: PayloadAction<Product>) => {
+      state.cart.push(action.payload);
+      saveState(state);
+    },
+    subtractFromCart: (state, action: PayloadAction<number>) => {
+      const productIdx = state.cart.findIndex(
+        (item) => item.id === action.payload
+      );
+
+      state.cart = [
+        ...state.cart.slice(0, productIdx),
+        ...state.cart.slice(productIdx + 1),
+      ];
+      
+      saveState(state);
+    },
+    removeFromCart: (state, action: PayloadAction<number>) => {
+      state.cart = state.cart.filter((item) => item.id !== action.payload);
+      saveState(state);
+    },
+    clearCart: (state) => {
+      state.cart = [];
+      saveState(state);
+    },
+  },
 });
 
 // Combinando o único slice
@@ -114,17 +128,18 @@ const rootReducer = appSlice.reducer;
 
 // Criando a store
 const store = configureStore({
-    reducer: rootReducer,
+  reducer: rootReducer,
 });
 
 // Exportando actions para uso em componentes
 export const {
-    setUser,
-    loggedIn,
-    loggedOut,
-    addToCart,
-    removeFromCart,
-    clearCart,
+  setUser,
+  loggedIn,
+  loggedOut,
+  addToCart,
+  removeFromCart,
+  subtractFromCart,
+  clearCart,
 } = appSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,63 +1,149 @@
 import { ProductionQuantityLimits, ShoppingCart } from "@mui/icons-material";
-import { Drawer, IconButton, Stack, Typography, useTheme } from "@mui/material";
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../../app/store";
+import {
+  Button,
+  Drawer,
+  IconButton,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import React, { useMemo, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  CartProduct,
+  Product,
+  RootState,
+  addToCart,
+  removeFromCart,
+  subtractFromCart,
+} from "../../app/store";
 import { tokens } from "../../theme";
+import CartItem from "./CartItem/CartItem";
+import "./index.css";
+
+const aggregateProducts = (products: Product[]): CartProduct[] => {
+  return Object.values(
+    products.reduce<Record<number, CartProduct>>((acc, item) => {
+      if (item.id in acc) {
+        acc[item.id].qty++;
+        return acc;
+      }
+
+      return {
+        ...acc,
+        [item.id]: {
+          ...item,
+          qty: 1,
+        },
+      };
+    }, [])
+  );
+};
+
+const getTotal = (cartProducts: CartProduct[]): string => {
+  return cartProducts
+    .reduce((acc, product) => {
+      return acc + product.qty * product.price;
+    }, 0)
+    .toFixed(2);
+};
 
 const Cart: React.FC = () => {
-    const [open, setOpen] = useState(false);
-    const cartItems = useSelector((state: RootState) => state.cart);
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
+  const [open, setOpen] = useState(false);
+  const cartItems = useSelector((state: RootState) => state.cart);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const cartProducts = useMemo(() => aggregateProducts(cartItems), [cartItems]);
+  const total = useMemo(() => getTotal(cartProducts), [cartProducts]);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    return (
-        <>
-            <IconButton color={"inherit"} onClick={() => setOpen(!open)}>
-                <ShoppingCart />
-            </IconButton>
-            <Drawer anchor={"right"} open={open} onClose={() => setOpen(false)}>
-                <Stack sx={{ padding: 4, width: 320 }}>
-                    <Stack
-                        direction={"row"}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                        gap={1}
-                    >
-                        <ShoppingCart />
-                        <Typography variant="h4" component="h1">
-                            Carrinho
-                        </Typography>
-                    </Stack>
-                    <Stack>
-                        {Object.keys(cartItems).length === 0 ? (
-                            <Stack
-                                py={4}
-                                alignItems={"center"}
-                                gap={1}
-                                sx={{ color: colors.neutral[9] }}
-                            >
-                                <ProductionQuantityLimits
-                                    sx={{
-                                        fontSize: 64,
-                                    }}
-                                />
-                                <Typography
-                                    variant="body1"
-                                    component="p"
-                                    color={"inherit"}
-                                >
-                                    Seu carrinho está vazio
-                                </Typography>
-                            </Stack>
-                        ) : (
-                            <Stack>{cartItems.toString()}</Stack>
-                        )}
-                    </Stack>
-                </Stack>
-            </Drawer>
-        </>
-    );
+  const handleAdd = (product: CartProduct) => {
+    dispatch(addToCart(product as Product));
+  };
+
+  const handleSubtract = (product: CartProduct) => {
+    dispatch(subtractFromCart(product.id));
+  };
+
+  const handleRemove = (product: CartProduct) => {
+    dispatch(removeFromCart(product.id));
+  };
+
+  return (
+    <>
+      <IconButton color={"inherit"} onClick={() => setOpen(!open)}>
+        <ShoppingCart />
+      </IconButton>
+      <Drawer anchor={"right"} open={open} onClose={() => setOpen(false)}>
+        <Stack sx={{ padding: 3, width: 320, height: "100vh" }}>
+          <Stack
+            direction={"row"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            gap={1}
+            sx={{ height: "60px" }}
+          >
+            <ShoppingCart />
+            <Typography variant="h4" component="h1">
+              Carrinho
+            </Typography>
+          </Stack>
+          <Stack
+            sx={{
+              height: "calc(100vh - 160px)",
+              overflowY: "auto",
+            }}
+          >
+            {cartProducts.length === 0 ? (
+              <Stack
+                py={4}
+                alignItems={"center"}
+                gap={1}
+                sx={{ color: colors.neutral[9] }}
+              >
+                <ProductionQuantityLimits
+                  sx={{
+                    fontSize: 64,
+                  }}
+                />
+                <Typography variant="body1" component="p" color={"inherit"}>
+                  Seu carrinho está vazio
+                </Typography>
+              </Stack>
+            ) : (
+              <Stack gap={3}>
+                {cartProducts.map((item) => (
+                  <CartItem
+                    key={item.id}
+                    item={item}
+                    onAdd={() => handleAdd(item)}
+                    onSubtract={() => handleSubtract(item)}
+                    onRemove={() => handleRemove(item)}
+                  />
+                ))}
+              </Stack>
+            )}
+          </Stack>
+          <Stack sx={{ height: "100px", justifyContent: "center" }}>
+            <Typography variant="h4" color="primary" fontWeight={"bold"}>
+              Total: R$ {total}
+            </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              onClick={() => navigate("/checkout")}
+              sx={{ width: "100%", marginTop: 2 }}
+            >
+              Finalizar compra
+            </Button>
+          </Stack>
+        </Stack>
+      </Drawer>
+    </>
+  );
 };
 
 export default Cart;
